735f25af66d3612563d09195ace1a3f8e9ab4149
hysseo <hysseo>
2008-10-30 03:09:03 +0000

    Beginning to add some function, with a text_t version...
    git-svn-id: http://svn.code.sf.net/p/materm/code/mrxvt05utf8@394 bb4227f5-41d7-4fa9-a6c6-f51fecf7dfea

--- src/tabbar.c
+++ src/tabbar.c
@@ -1640,6 +1640,57 @@ rxvt_remove_page (rxvt_t* r, short page)
 
 
 /*
+ * Set new title for a tab.
+ * Same as rxvt_tabbar_set_title, but using a char* as parameter. 
+ */
+/* EXTPROTO */
+void
+rxvt_tabbar_set_title_from_char (rxvt_t* r, short page, const char TAINTED * str)
+{
+    char UNTAINTED *	    n_title;
+
+    assert (str);
+    assert (page >= 0 && page <= LTAB(r));
+    assert (PVTS(r, page)->tab_title);
+    int str_len = STRLEN (str);
+
+    n_title = STRNDUP (str, MAX_TAB_TXT);
+    /*
+     * If strdup succeeds, set new title
+     */
+    if (NULL != n_title)
+    {
+	rxvt_free (PVTS(r, page)->tab_title);
+	//PVTS(r, page)->tab_title = n_title;
+	//PVTS(r, page)->tab_title_length = str_len;
+	PVTS(r, page)->tab_title_length = from_char_to_text (r, n_title, str_len, PVTS(r, page)->tab_title);
+
+	/* Compute the new width of the tab */
+	PVTS(r, page)->tab_width = rxvt_tab_width (r, n_title);
+    }
+    else
+	rxvt_free (n_title);
+
+    /*
+     * If visible tab's title is changed, refresh tab bar
+     */
+    if (page >= FVTAB(r) && page <= LVTAB(r))
+    {
+	/* adjust visible tabs */
+	rxvt_tabbar_set_visible_tabs (r, True);
+	refresh_tabbar_tab (r, page);
+    }
+
+    /* synchronize terminal title with active tab title */
+    if (ISSET_OPTION(r, Opt2_syncTabTitle) &&
+	(page == ATAB(r)))
+	sync_tab_title( r, ATAB(r) );
+
+    /* synchronize icon name to tab title */
+    if (ISSET_OPTION(r, Opt2_syncTabIcon) && page == ATAB(r))
+	rxvt_set_icon_name (r, (const text_t*) PVTS(r, ATAB(r))->tab_title);
+}
+/*
  * Set new title for a tab
  */
 /* EXTPROTO */
